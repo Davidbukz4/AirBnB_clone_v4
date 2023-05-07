b9e92927c107ba0c89433db018a56283018b2866
David Egwuatu <davidegwuatu2@gmail.com>
2023-05-07 23:31:49 +0000

    modified task 4

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -42,49 +42,6 @@ def hbnb():
                            cache_id=uuid.uuid4())
 
 
-@app_views.route("/places_search", methods=["POST"],
-                 strict_slashes=False)
-@swag_from('documentation/place/put_place.yml', methods=['PUT'])
-def places_search():
-    """Retrieves all Place objects depending of the body of the request"""
-    body = request.get_json()
-    if type(body) != dict:
-        abort(400, description="Not a JSON")
-    id_states = body.get("states", [])
-    id_cities = body.get("cities", [])
-    id_amenities = body.get("amenities", [])
-    places = []
-    if id_states == id_cities == []:
-        places = storage.all(Place).values()
-    else:
-        states = [
-            storage.get(State, _id) for _id in id_states
-            if storage.get(State, _id)
-        ]
-        cities = [city for state in states for city in state.cities]
-        cities += [
-            storage.get(City, _id) for _id in id_cities
-            if storage.get(City, _id)
-        ]
-        cities = list(set(cities))
-        places = [place for city in cities for place in city.places]
-
-    amenities = [
-        storage.get(Amenity, _id) for _id in id_amenities
-        if storage.get(Amenity, _id)
-    ]
-
-    res = []
-    for place in places:
-        res.append(place.to_dict())
-        for amenity in amenities:
-            if amenity not in place.amenities:
-                res.pop()
-                break
-
-    return jsonify(res)
-
-
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
